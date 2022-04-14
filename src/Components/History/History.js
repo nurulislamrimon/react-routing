7ee91d8c22bb56ce5f44a1a9d16bc93022fad59a
nurulislamrimon <nurulislamrimon@gmail.com>
2022-04-14 22:58:02 +0600

    data loaded

--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const History = () => {
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(res => res.json())
+            .then(datas => console.log(datas))
+    }, [])
     return (
         <div>
             <h1>Hello History page </h1>
