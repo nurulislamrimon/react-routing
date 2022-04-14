b1e8b503d7deeb57bd1e4634f1c0058cd2c1bebc
nurulislamrimon <nurulislamrimon@gmail.com>
2022-04-15 01:09:16 +0600

    nav bar ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Components/About/About';
 import History from './Components/History/History';
@@ -7,11 +7,17 @@ import Home from './Components/Home/Home';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/history">History</Link>
+      </nav>
       <Routes>
         <Route path='/' element={<><Home></Home><About></About><History></History></>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/history' element={<History></History>}></Route>
+        <Route path='*' element={<h1>No Page Found</h1>}></Route>
       </Routes>
 
     </div>
--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import User from '../User/User';
 
 const History = () => {
-    const [user, setUser] = useState([]);
+    const [users, setUsers] = useState([]);
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(res => res.json())
-            .then(datas => console.log(datas))
+            .then(datas => setUsers(datas))
     }, [])
+    console.log(users);
+
     return (
         <div>
-            <h1>Hello History page </h1>
-            <p>Hello History page </p>
-            <p>Hello History page </p>
-            <p>Hello History page </p>
+            <h1>User History page </h1>
+            {users.map(user => <User user={user} key={user.id}></User>)}
         </div>
     );
 };
